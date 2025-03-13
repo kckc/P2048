dcd8dda3514cb42166f2b81654d639629a605c04
Kenneth Chau <kckc@users.noreply.github.com>
2025-03-13 15:01:50 +0000

    fix gitignore

--- layer.py
+++ layer.py
@@ -50,3 +50,56 @@ class FullyConnectedLayer:
         self.weights = self.dweights
         self.biases = self.dbiases
         return self.dinputs
+
+class Link:
+    def __init__(self, src, dest, weight):
+        self.src = src
+        self.dest = dest
+        self.weight = weight
+    def getValue(self):
+        return self.src.getValue() * self.weight
+    def grad(self, n):
+        return 2/n*self.src.value
+    def learn(self,  n, dest_grad_times_error):
+        self.weight -= self.grad(n) * dest_grad_times_error * Node.l_rate
+        self.src.back(self.grad(n) * dest_grad_times_error)
+
+class Node:
+    l_rate=0.01
+    def __init__(self):
+        self.value = None
+        self.links = []
+        self.bias = Node.initW()
+    def initW():
+        # return 1
+        return random.uniform(-1, 1)
+    def link(self, node):
+        l = Link(self, node, Node.initW())
+        node.links.append(l)
+    def cal(self):
+        self.value = sum([l.getValue() for l in self.links]) + self.bias
+        return self.value
+    def getValue(self):
+        return self.value if self.value is not None else self.cal()
+    def back(self, expected):
+        self.bias -= (self.value - expected) * Node.l_rate
+        for l in self.links:
+            l.learn(len(self.links), self.value - expected)
+
+
+class Layer:
+    def __init__(self, in_dim, out_dim, inlayer = None):
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+        self.inlayer = [Node() for i in range(in_dim)] if inlayer is None else inlayer
+        self.outlayer = [Node() for i in range(out_dim)]
+        for i in self.inlayer:
+            for j in self.outlayer:
+                i.link(j)
+    def forward(self):
+        return [n.getValue() for n in self.outlayer]
+    def backward(self, dvalues):
+        [n.back(dvalues[i]) for i, n in enumerate(self.outlayer)]
+    def print(self):
+        for i in self.outlayer:
+            print([link.weight for link in i.links], i.bias)
--- main.py
+++ main.py
@@ -1,28 +1,45 @@
 import random
 from game import Game
-from model import AllConnectedModel, RandomModel
+from model import AllConnectedModel, RandomModel, UserModel
+from layer import Node, Layer
 
-m = AllConnectedModel()
+l1 = Layer(16, 16)
+for n in l1.inlayer:
+    n.value = 0.5
+l1.print()
+l2 = Layer(16, 4, l1.outlayer)
 
-def play(model):
-  print("New Game")
-  g = Game()
-  while not g.finished():
-    # g.print()
-    # g.wait()
-    # g.action(
+exp = [0,1,0,0]
+
+for i in range(2):
+  print("----------------------")
+  l2.forward()
+  l2.backward(exp)
+  l2.print()
+  print([l.value for l in l2.outlayer])
+
+def main():
+  m = AllConnectedModel()
+  def play(model):
+    print("New Game")
+    g = Game()
+    while not g.finished():
+      g.print()
+      # g.wait()
+      # g.action(
       # random.choice([0,1,2,3]))
-    inpt = [v - 0.5 for v in g.state]
-    a = model.run(inpt)
-    score_diff = g.action(a)
-    m.learn(inpt, a, score_diff)
+      # inpt = [v - 0.5 for v in g.state]
+      inpt = g.state
+      a = model.run(inpt)
+      score_diff = g.action(a)
+      m.learn(inpt, a, score_diff)
     g.print()
-  g.print()
-  print(f"Game finished, moves: {g.moves}, score: {g.score}, max: {max(g.state)} ")
-
-for i in range(10):
-  play(m)
-
+    print(
+        f"Game finished, moves: {g.moves}, score: {g.score}, max: {max(g.state)} "
+    )
+  
+  for i in range(10):
+    play(UserModel())
 
 # while game not finished
 # if prev state != curr state
@@ -31,6 +48,3 @@ for i in range(10):
 # wait for input
 # left:
 #   remove 0 on left side, if same number then add it
-
-
-
